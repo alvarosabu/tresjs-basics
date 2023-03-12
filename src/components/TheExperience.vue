b3d07091ba9cecf50b0882714e08c8450d7a425a
alvarosabu <alvaro.saburido@gmail.com>
2023-03-12 11:24:02 +0100

    feat: Orbit controls

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 import { useRenderLoop } from '@tresjs/core';
-import { ref, watch } from 'vue';
+import { OrbitControls } from '@tresjs/cientos';
+import { ref,  } from 'vue';
 
-const boxRef = ref();
-const boxPosition = ref([0, 0, 0]);
+const torusRef = ref();
+const torusPosition = ref([0, 0, 0]);
 
 const { onLoop } = useRenderLoop();
 
 onLoop(({ delta, elapsed}) => {
-    if (!boxRef.value) return;
-
-    boxPosition.value[0] += delta;
-
-    /* boxRef.value.rotation.z += delta;
-    boxRef.value.rotation.y = elapsed; */
+    if (!torusRef.value) return;
 });
 </script>
 <template>
@@ -21,9 +17,10 @@ onLoop(({ delta, elapsed}) => {
         <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]"
             :position="[3, 1, 4]" :look-at="[0, 0, 0]"
         />
+        <OrbitControls/>
         <TresScene>
-            <TresMesh ref="boxRef" :position="boxPosition">
-                <TresBoxGeometry :args="[1,1,1]" />
+            <TresMesh ref="torusRef" :position="torusPosition">
+                <TresTorusKnotGeometry :args="[1, 0.4, 100, 16]" />
                 <TresMeshNormalMaterial  />
             </TresMesh>
         </TresScene>
