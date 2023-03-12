aa2aec2814a902e926d9e0f0ccf7866daa7aab75
alvarosabu <alvaro.saburido@gmail.com>
2023-03-12 10:25:47 +0100

    chore: update deps

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
+import { useRenderLoop } from '@tresjs/core';
+import { ref, watch } from 'vue';
 
+const boxRef = ref();
+const boxPosition = ref([0, 0, 0]);
+
+const { onLoop } = useRenderLoop();
+
+onLoop(({ delta, elapsed}) => {
+    if (!boxRef.value) return;
+
+    boxPosition.value[0] += delta;
+
+    /* boxRef.value.rotation.z += delta;
+    boxRef.value.rotation.y = elapsed; */
+});
 </script>
 <template>
     <TresCanvas window-size>
@@ -7,7 +22,7 @@
             :position="[3, 1, 4]" :look-at="[0, 0, 0]"
         />
         <TresScene>
-            <TresMesh :position="[0, 2, 0]" :rotation="[0, Math.PI / 6,Math.PI / 6]">
+            <TresMesh ref="boxRef" :position="boxPosition">
                 <TresBoxGeometry :args="[1,1,1]" />
                 <TresMeshNormalMaterial  />
             </TresMesh>
