6c06200aa102bb9e74d926dc60fdb0c4da13781f
alvarosabu <alvaro.saburido@gmail.com>
2023-06-18 18:13:57 +0200

    feat: `useRenderLoop` and template ref to animate

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
 import { OrbitControls } from '@tresjs/cientos'
+import { shallowRef } from 'vue'
+
+// Use TemplateRef to access the THREE instance you want to animate
+const cubeRef = shallowRef<THREE.Mesh>()
+const { onLoop } = useRenderLoop()
+
+// Use onLoop to run a function on every frame
+onLoop(({ delta, elapsed }) => {
+  // Use the THREE instance to animate
+  if (!cubeRef.value) return
+
+  cubeRef.value.rotation.y += delta
+  cubeRef.value.rotation.z = elapsed
+
+  // Animate using trigonometry
+  cubeRef.value.position.y = Math.sin(elapsed) * 2
+  cubeRef.value.scale.set(
+    Math.sin(elapsed) * 2,
+    Math.sin(elapsed) * 2,
+    Math.sin(elapsed) * 2,
+  )
+
+})
 </script>
 
 <template>
@@ -8,7 +31,7 @@ import { OrbitControls } from '@tresjs/cientos'
     <TresPerspectiveCamera />
     <OrbitControls />
     <TresScene>
-      <TresMesh>
+      <TresMesh ref="cubeRef">
         <TresBoxGeometry :args="[1, 1, 1]" />
         <TresMeshNormalMaterial />
       </TresMesh>
